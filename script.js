be94ed787332f9880f11c6213399acf1b3ed0d02
Jonathan <jonathanbek93@gmail.com>
2019-07-05 16:29:37 +0800

    completed base game

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 //option to set numOfPanels with input prompt
 var numOfPanels = 3
+var cell = null;
+var previousCell = null;
+var backImage = "url('poring.jpg')";
+// background-image: url('memeface.png');
+
 window.onload = function(){
     createBoard();
+    nextPopup();
 }
-
-
-
 
 var createBoard = function(){
     for (i = 1; i < 4; i += 1){ //using 4 instead of 3 so I don't have to +1
@@ -18,7 +21,6 @@ var createBoard = function(){
             col.classList.add(`row${i}`);
             col.setAttribute("id", "box");
             col.addEventListener("click", hit)
-            col.textContent = "TEST";
             row.appendChild(col);
         }
     }
@@ -26,10 +28,45 @@ var createBoard = function(){
 //when i run createBox();, it should create a 3 by 3 grid, and attach event listener for hit() on click to each
 
 var hit = function(){
-    console.log("hit!")
+    console.log("hit!");
+    // getRandomCell();
+    clickTarget(event);
+    nextPopup();
 }
 
-var popup = function(){
-    var randomNum = Math.floor((Math.random() * 9) + 1);
-    console.log(randomNum);
+
+//getRandomCell works!
+var getRandomCell = function(){
+    var randomX = Math.floor((Math.random() * 3) + 1);
+    console.log("xcoord" + randomX);
+    var randomY = Math.floor((Math.random() * 3) + 1);
+    console.log("ycoord" + randomY);
+    cell = document.querySelector(`.row${randomX}.col${randomY}`);
+    console.log("Cell: " + cell);
+}
+
+var clickTarget = function(event){
+    if (previousCell){
+        previousCell.style.backgroundImage = null;
+        previousCell.innerHTML = null;
+    }
+    errorCheck(event);
+    getRandomCell();
+    event.target.style.backgroundImage = backImage;
+    event.target.innerHTML === null;
+    previousCell = event.target;
+}
+
+//combine with click target
+var nextPopup = function(){
+    getRandomCell();
+    cell.innerHTML = "Click here!";
+}
+
+var errorCheck = function(event){
+    if (event.target === cell){
+        console.log("Next!");
+    } else {
+        console.log("You Lost!");
+    }
 }
