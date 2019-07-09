bd7591d2f450a349a78a374f87a104bb90974b32
Jonathan <jonathanbek93@gmail.com>
2019-07-09 17:36:58 +0800

    added kanjiDetails, organised the css a little

--- script.js
+++ script.js
@@ -43,9 +43,9 @@ var responseHandler = function(){
     var kanjiIndex = Math.floor(Math.random() * response.length);
     randomKanji = response[kanjiIndex];
     let target = document.querySelector('.goalkanji');
-    target.innerHTML = "Target Kanji: " + randomKanji;
-    // makeDiv();
+    target.innerHTML = randomKanji;
     loopMakeDiv();
+    getKanjiDetails();
 }
 
 
@@ -75,6 +75,34 @@ var getFakeKanji = function(){
     console.log(fakeKanji);
 }
 //----------------------------
+
+var kanjiDetails = "";
+var kunyomi = "";
+var onyomi = "";
+var kanjiMeanings = "";
+var jlptLevel = "";
+//test get kanji details
+var responseHandler3 = function(){
+    var response = JSON.parse(this.responseText);
+    // var kanjiIndex = Math.floor(Math.random() * response.length);
+    kanjiDetails = response;
+    kunyomi = kanjiDetails.kun_readings;
+    onyomi = kanjiDetails.on_readings;
+    kanjiMeanings = kanjiDetails.meanings;
+    jlptLevel = kanjiDetails.jlpt;
+    document.querySelector(".kunyomi").innerHTML = `Kun readings:<br />${kunyomi}`;
+    document.querySelector(".onyomi").innerHTML = `On readings:<br />${onyomi}`;
+    document.querySelector(".kanjiMeanings").innerHTML = `Meanings:<br />${kanjiMeanings}`;
+    // document.querySelector(".jlptLevel").innerHTML = `JLPT Level: ${jlptLevel}`;
+}
+
+
+var getKanjiDetails = function(){
+    var request = new XMLHttpRequest();
+    request.addEventListener("load", responseHandler3);
+    request.open("GET", `https://kanjiapi.dev/v1/kanji/${randomKanji}`);
+    request.send();
+}
 
 //makeDiv in getrandomkanji
 window.onload = function(){
@@ -345,99 +373,3 @@ var minusScoreText = function(){
 //site used: https://kanjiapi.dev/
 //peculiarities: search by grades 1-6 (elementary, 1000+) and 8 (all of secondary, another 1000+)
 //eg:"https://kanjiapi.dev/v1/kanji/grade-8"
-
-
-///USING THE CODE BELOW
-
-// var response = "";
-// var randomKanji = "";
-// var searchGrade = 6; //vary this on input!
-
-// var responseHandler = function(){
-//     response = JSON.parse(this.responseText);
-//     // console.log(response);
-//     //include 0 up to response.length minus 1
-//     var kanjiIndex = Math.floor(Math.random() * response.length);
-//     randomKanji = response[kanjiIndex];
-// }
-
-// var getRandomKanji = function(){
-//     var request = new XMLHttpRequest();
-//     //your request does responseHandler on load
-//     request.addEventListener("load", responseHandler);
-//     //open readies the system, and targets the URL
-//     request.open("GET", `https://kanjiapi.dev/v1/kanji/grade-${searchGrade}`);
-//     request.send();
-// }
-
-
-//OLD CREATEBOARD
-// var createBoard = function(){
-//     // getRandomKanji();
-//     console.log(randomKanji);
-//     for (i = 1; i < 4; i += 1){ //using 4 instead of 3 so I don't have to +1
-//         var row = document.createElement('div');
-//         row.setAttribute("class", "row")
-//         document.querySelector('.main').appendChild(row);
-//         for (j = 1; j < 4; j += 1){
-//             var col = document.createElement('span');
-//             col.setAttribute("class", `col${j}`);
-//             col.classList.add(`row${i}`);
-//             col.setAttribute("id", "box");
-//             col.addEventListener("click", clickTarget);
-//             row.appendChild(col);
-//         }
-//     }
-// }
-
-
-//original clickTarget
-// var clickTarget = function(event){
-//     errorCheck(event);
-//     event.target.style.backgroundImage = backImage;
-//     event.target.innerHTML === "";
-//     previousCell = event.target;
-//     cell = "";
-//     addScore();
-//     pushScore();
-//     nextPopup();
-// }
-
-// var getRandomCell = function(){
-//     var randomX = Math.floor((Math.random() * 3) + 1);
-//     var randomY = Math.floor((Math.random() * 3) + 1);
-//     cell = document.querySelector(`.row${randomX}.col${randomY}`);
-// }
-
-
-//original
-// var clickTarget = function(event){
-//     errorCheck(event)
-//     console.log("in clicktarget" + randomKanji)//delete
-//     previousCell = event.target;
-//     addScore();
-//     console.log("code reached here!")
-//     pushScore();
-//     nextPopup();
-// }
-
-//original nextPopup
-// var nextPopup = function(){
-//     if (previousCell){
-//         previousCell.style.backgroundImage = "";
-//         previousCell.innerHTML = "";
-//     }
-//     getRandomCell();
-//     cell.innerHTML = "Click here!";
-// }
-
-
-
-// var errorCheck = function(event){
-//     if (event.target === cell){
-//         console.log("Next!");
-//     } else {
-//         console.log("You Lost!");
-//         resetBoard();
-//     }
-// }
